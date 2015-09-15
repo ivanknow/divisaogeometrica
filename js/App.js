50f3283c497efb3888d7245f19920b66ce80c214
Ivan Rodrigues <ivanknow@gmail.com>
2015-09-15 16:59:34 +0000

    fazendo imagem grande e trao

--- js/App.js
+++ js/App.js
@@ -6,9 +6,22 @@
 App = {
 		
 init : function() {//init
-
+ 
+ $( "#board" ).on( "click", ".aresta", function(evt) {
+   alert("vai rolar tracinho"+evt);
    
- $(".shape").click(function(evt){
+  // $("#board svg").append("<line x1='10' y1='10' x2='200' y2='200' style='stroke:rgb(255,0,0);stroke-width:2'></line>");
+  
+   var newLine = document.createElementNS('http://www.w3.org/2000/svg','line');
+    newLine.setAttribute('class','line');
+    newLine.setAttribute('x1','0');
+    newLine.setAttribute('y1','0');
+    newLine.setAttribute('x2','300');
+    newLine.setAttribute('y2','300');
+    $("#board svg").append(newLine);
+});
+
+$(".shape").click(function(evt){
 	       /* var cor = $(this).css("fill");
 	        $("#board").html(cor);
 	        var e = evt.target;
@@ -18,8 +31,13 @@ init : function() {//init
     alert("x: "+x+" y:"+y);*/
    // alert("");
     var content = $("<svg   x='0' y='0' class='main figurinhas'/>").append($(this).clone()).html();
-    var item = "<svg   x='0' y='0' class='main figurinhas'>"+content+"</svg>";
+    content+= "<circle cx='10' cy='10' r='9' fill='black' class='aresta' stroke-width='10'/>";
+   content+="<line x1='10' y1='10' x2='20' y2='20' style='stroke:rgb(255,0,0);stroke-width:2' />";
+    var item = "<svg   x='0' y='0' class='main figura'>"+content+"</svg>";
+    
     $("#board").html(item);
+    
+    $("#board  rect").attr("class",'figura');
 });
 
 }//fim init
