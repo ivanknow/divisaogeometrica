71ec52c989dba042b01ac7e3d7e168589e09b587
Ivan Rodrigues <ivanknow@gmail.com>
2016-03-24 23:36:08 -0300

    ponto medio do triangulo

--- js/App.Modelos.Figuras.js
+++ js/App.Modelos.Figuras.js
@@ -17,12 +17,14 @@ App.Figuras = {
 		this.vertices = [
 			{id:"v0",x:0,y:400},
 		 	{id:"v1",x:200,y : 0},
-			{id : "v2",	x : 400,y : 400}
+			{id : "v2",	x : 400,y : 400},
+			{id : "v3",	x : 200,y : 400}
 		];
 		this.arestas = 	[
 			{vi : "v0",	vf : "v1",	d : 400	},
 			{	vi : "v1",vf : "v2",d : 400},
-			{	vi : "v2",	vf : "v0",d : 400	}
+			{	vi : "v2",	vf : "v3",d : 400	},
+			{	vi : "v3",	vf : "v0",d : 400	}
 		];
 
 		break;
