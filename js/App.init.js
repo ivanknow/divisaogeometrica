029297825e2af4804197f28ae82225a535e0c347
Ivan Rodrigues <ivanknow@gmail.com>
2016-03-31 14:25:53 -0300

    faltando tratar os colineares

--- js/App.init.js
+++ js/App.init.js
@@ -9,7 +9,7 @@ App.init = function() {
 		//mouse
 		$("#board").on("vmousedown", ".vertice", App.events.onMouseOverVerticeFindSecond);
 
-		$("#board").on("click",".vertice",App.events.onClickVerticeSelectFirst);
+		$("#board").on("vclick",".vertice",App.events.onClickVerticeSelectFirst);
 
 		//$("#board").on("vmousemove",	".main", App.events.onMouseMoveOnMainSVG);
 
